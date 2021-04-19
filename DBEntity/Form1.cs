b28b655e4e6ed7dab72d46ddb0b4e0f6a4b83939
Copying-Ninja <55594704+Copying-Ninja@users.noreply.github.com>
2021-04-19 03:45:41 +0300

    Змінено з браузера
    Змінено з браузера

--- DBEntity/Form1.cs
+++ DBEntity/Form1.cs
@@ -131,7 +131,7 @@ namespace DBEntity
             Clear();
             PopulateDataGridView();
             MessageBox.Show("Зміни_1");
-            Console.WriteLine("Зміни_2");
+            Console.WriteLine("Змінено з браузера");
         }
     }
 }
